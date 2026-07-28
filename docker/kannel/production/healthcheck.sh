5b6189cf2b4b0a35854f756bf065d029aef55122
Dylan Steele <dylansteele57@gmail.com>
2026-07-28 08:22:30 -0400

    fix(infra): authenticate Kannel health probes (#478)

--- docker/kannel/production/healthcheck.sh
+++ docker/kannel/production/healthcheck.sh
@@ -2,6 +2,7 @@
 set -eu
 
 runtime_dir=/run/kannel
+status_secret_file=${KANNEL_STATUS_PASSWORD_FILE:-/run/secrets/kannel_status_password}
 for process_name in bearerbox wapbox; do
   pid_file=${runtime_dir}/${process_name}.pid
   test -r "$pid_file"
@@ -12,5 +13,7 @@ for process_name in bearerbox wapbox; do
   kill -0 "$pid" 2>/dev/null
 done
 
+test -r "$status_secret_file"
 curl -sS --output /dev/null --connect-timeout 1 --max-time 3 \
+  --get --data-urlencode "password@$status_secret_file" \
   'http://127.0.0.1:13000/status'
